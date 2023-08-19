966b0c1476f91667fc788ef8595f6df44cc7e170
Hassan Azeez Adeola <mo_lary@yahoo.com>
2023-08-19 00:42:19 -0400

    Initial Commit

---
+++ mostore/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mostore
+//
+//  Created by Azeez Adeola Hassan on 2023-08-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mostore/mostoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  mostoreApp.swift
+//  mostore
+//
+//  Created by Azeez Adeola Hassan on 2023-08-19.
+//
+
+import SwiftUI
+
+@main
+struct mostoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
